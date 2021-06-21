559f177c6f462191182000acda3b4a2f81e4d615
yeon <jryoun0404@gmail.com>
2021-06-22 01:04:38 +0900

    [#3] feat: Add LocationManager class

---
+++ WeatherApp/WeatherApp/Utilities/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by Yeon on 2021/06/21.
+//
+
+import UIKit
+import CoreLocation
+
+final class LocationManager {
+    let locationManger = CLLocationManager()
+}
