78a699fa3b63f86f58bb3c651198bd33cf5441e4
yeon <jryoun0404@gmail.com>
2021-06-26 01:14:18 +0900

    [#2] chore: Change intent in file

--- WeatherApp/WeatherApp/Network/NetworkManager.swift
+++ WeatherApp/WeatherApp/Network/NetworkManager.swift
@@ -32,23 +32,23 @@ struct NetworkManager {
     }
     
     private func communicateToServer(with request: URLRequest, completion: @escaping resultHandler) {
-            let session: URLSession = URLSession.shared
-            let dataTask: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-                
-                if let error = error {
-                    return completion(.failure(error))
-                }
-                
-                guard let httpResponse = response as? HTTPURLResponse,
-                      (200...299).contains(httpResponse.statusCode) else {
-                    return completion(.failure(Error))
-                }
-                
-                guard let data = data else {
-                    return completion(.failure(Error))
-                }
-                return completion(.success(data))
+        let session: URLSession = URLSession.shared
+        let dataTask: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                return completion(.failure(error))
             }
-            dataTask.resume()
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                return completion(.failure(Error))
+            }
+            
+            guard let data = data else {
+                return completion(.failure(Error))
+            }
+            return completion(.success(data))
         }
+        dataTask.resume()
+    }
 }
