5c2e2b2719752e51090f4e669280d41ada8343c3
yeon <jryoun0404@gmail.com>
2021-06-29 00:48:44 +0900

    [#6] Add WeatherTableViewCell file

---
+++ WeatherApp/WeatherApp/View/WeatherTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Yeon on 2021/06/29.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
