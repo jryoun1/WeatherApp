f51e03f0aaead6fe2a8bfc1e8948cc65cb5f9946
yeon <jryoun0404@gmail.com>
2021-06-30 01:50:14 +0900

    [#8] Create WeatherTableViewHeaderView file

---
+++ WeatherApp/WeatherApp/View/WeatherTableViewHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherTableViewHeaderView.swift
+//  WeatherApp
+//
+//  Created by Yeon on 2021/06/30.
+//
+
+import UIKit
+
+final class WeatherTableViewHeaderView: UITableViewHeaderFooterView {
+
+}
