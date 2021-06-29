a05cd77c81983ba728b3ed605cf71077ca6dde04
yeon <jryoun0404@gmail.com>
2021-06-29 22:36:53 +0900

    [#6] feat: Add configureLayout method

--- WeatherApp/WeatherApp/View/WeatherTableViewCell.swift
+++ WeatherApp/WeatherApp/View/WeatherTableViewCell.swift
@@ -33,6 +33,7 @@ final class WeatherTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupWeatherTableViewCell()
+        configureLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -43,5 +44,27 @@ final class WeatherTableViewCell: UITableViewCell {
         contentView.addSubview(dateTimeLabel)
         contentView.addSubview(currentTemperatureLabel)
         contentView.addSubview(weatherImageView)
+    }
+    
+    private func configureLayout() {
+        dateTimeLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        dateTimeLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        
+        currentTemperatureLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        currentTemperatureLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        NSLayoutConstraint.activate([
+            dateTimeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            dateTimeLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            currentTemperatureLabel.leadingAnchor.constraint(equalTo: dateTimeLabel.trailingAnchor),
+            currentTemperatureLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            weatherImageView.leadingAnchor.constraint(equalTo: currentTemperatureLabel.trailingAnchor, constant: 10),
+            weatherImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            weatherImageView.widthAnchor.constraint(equalToConstant: 35),
+            weatherImageView.heightAnchor.constraint(equalTo: weatherImageView.widthAnchor),
+            weatherImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
+        ])
     }
 }
