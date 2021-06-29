2ed683acc5e07fb3d682a17896b30af60cd5c37d
yeon <jryoun0404@gmail.com>
2021-06-29 22:32:48 +0900

    [#6] feat: Add dateTimeLabel property

--- WeatherApp/WeatherApp/View/WeatherTableViewCell.swift
+++ WeatherApp/WeatherApp/View/WeatherTableViewCell.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 final class WeatherTableViewCell: UITableViewCell {
-
+    private var dateTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = .preferredFont(forTextStyle: .body)
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
