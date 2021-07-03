c2d62dfe6efa75d4db705812581e15cb8091a85b
yeon <jryoun0404@gmail.com>
2021-07-04 00:11:40 +0900

    [#7] feat: Implement UITableViewDatasource cellForRowAt method

--- WeatherApp/WeatherApp/Controller/MainViewController.swift
+++ WeatherApp/WeatherApp/Controller/MainViewController.swift
@@ -11,7 +11,7 @@ final class MainViewController: UIViewController {
     private let weatherTableView = UITableView(frame: CGRect.zero, style: .grouped)
     private var currentWeather: CurrentWeather?
     private var forecastWeatherList: ForecastWeatherList?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupWeatherTableView()
@@ -36,5 +36,13 @@ final class MainViewController: UIViewController {
 extension MainViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return forecastWeatherList?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let weatherTableViewCell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.cellID, for: indexPath) as? WeatherTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return weatherTableViewCell
     }
 }
