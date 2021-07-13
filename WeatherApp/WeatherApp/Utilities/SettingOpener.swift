07f61027c41f955fc6998f20980d26e793ca064c
yeon <jryoun0404@gmail.com>
2021-07-14 00:18:59 +0900

    feat: Create URLOpening protocol to make SettingOpener testable

--- WeatherApp/WeatherApp/Utilities/SettingOpener.swift
+++ WeatherApp/WeatherApp/Utilities/SettingOpener.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+protocol URLOpening {
+    func open(_ url: URL, options: [UIApplication.OpenExternalURLOptionsKey : Any], completionHandler completion: ((Bool) -> Void)?)
+}
+
+extension UIApplication: URLOpening { }
+
 final class SettingOpener {
 
 }
