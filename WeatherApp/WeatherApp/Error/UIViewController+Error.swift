c1b7835674ecd00fd56aa9152a4db66099462692
yeon <jryoun0404@gmail.com>
2021-07-14 00:08:48 +0900

    [#4] feat: Add showAlert method

--- WeatherApp/WeatherApp/Error/UIViewController+Error.swift
+++ WeatherApp/WeatherApp/Error/UIViewController+Error.swift
@@ -7,4 +7,15 @@
 
 import UIKit
 
-extension UIViewController { }
+extension UIViewController {
+    private func showAlert(about error: WeatherError) {
+        let alert = UIAlertController(title: "에러 발생",
+                                      message: "\(error.localizedDescription) 앱을 다시 실행해주세요",
+                                      preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
