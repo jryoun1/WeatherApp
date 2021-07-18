8c2f62623f491d20f34ecc5bb372fb6f422eda4a
yeon <jryoun0404@gmail.com>
2021-07-18 20:53:05 +0900

    [#9] feat: Extension UIImageView have loadImage method applying memory cache

--- WeatherApp/WeatherApp/Utilities/ImageCacheManager.swift
+++ WeatherApp/WeatherApp/Utilities/ImageCacheManager.swift
@@ -11,3 +11,28 @@ final class ImageCacheManager {
     static let shared = NSCache<NSString, UIImage>()
     private init() {}
 }
+
+extension UIImageView {
+    func loadImage(_ imageID: String) {
+        let cacheKey = NSString(string: imageID)
+        if let cachedImage = ImageCacheManager.shared.object(forKey: cacheKey) {
+            self.image = cachedImage
+            return
+        }
+        
+        NetworkManager.shared.loadImage(imageID: imageID) { result in
+            switch result {
+            case .success(let data):
+                if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async { [weak self] in
+                        ImageCacheManager.shared.setObject(image, forKey: cacheKey)
+                        self?.image = image
+                    }
+                }
+            case .failure:
+                self.image = UIImage()
+                return
+            }
+        }
+    }
+}
