3aaa63dc57d0b6785fa9659b18631e86dfdf9a41
yeon <jryoun0404@gmail.com>
2021-07-23 13:24:19 +0900

    [#5] Create SettingOpenerTests file

---
+++ WeatherApp/WeatherAppTests/SettingOpenerTests.swift
@@ -0,0 +1,13 @@
+//
+//  SettingOpenerTests.swift
+//  WeatherAppTests
+//
+//  Created by Yeon on 2021/07/23.
+//
+
+import XCTest
+@testable import WeatherApp
+
+final class SettingOpenerTests: XCTestCase {
+
+}
